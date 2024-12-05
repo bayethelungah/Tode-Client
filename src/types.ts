e93b4bef1a26d6c3d15c2bc196303f9aedfb7f6a
Bayethe Lungah <bayethelungah@Bayethes-MacBook-Pro.local>
2024-12-05 15:02:20 -0500

    created more generic server messages for application

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,31 @@
-export interface DependencyNode {
+export interface DependencyNode extends TodeResponseMessageBody {
     name: string;
     version: string;
-    dependencies: DependencyNode[] | null;
+    status: string;
+    dependencies: { [key: string]: DependencyNode } | undefined;
 }
+
+
+export enum StatusColor {
+    ok = "bg-green-300",
+    outdated = "bg-yellow-300",
+    vulnerable = "bg-red-300",
+    default = "bg-blue-300"
+}
+
+
+export interface TodeRequestMessage {
+    body: string;
+}
+
+export interface TodeResponseMessage {
+    body: TodeResponseMessageBody | null;
+    errorMessage: string | null;   
+}
+
+
+export interface TodeResponseMessageBody {
+     
+}
+
+
