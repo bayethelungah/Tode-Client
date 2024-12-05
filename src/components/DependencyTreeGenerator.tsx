7d713ba8992f85e5ff9cd77df3ea9f3453d028e1
Bayethe Lungah <bayethelungah@Bayethes-MacBook-Pro.local>
2024-12-05 15:01:02 -0500

    removed unused react import

--- src/components/DependencyTreeGenerator.tsx
+++ src/components/DependencyTreeGenerator.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 import { createRoot } from 'react-dom/client';
 import { PackageNode } from './PackageNode';
